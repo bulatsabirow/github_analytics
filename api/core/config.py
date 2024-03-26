1f6d9a829488ed42a4a0cc38cd10e95d0cc69898
bulatsabirow <bulatsabirow@gmail.com>
2024-03-26 14:53:03 +0300

    + init database service

---
+++ api/core/config.py
@@ -0,0 +1,20 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "postgres"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5438
+    DB_NAME: str = "postgres"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=os.path.join(".env"), env_file_encoding="utf-8", extra="ignore")
+
+
+settings = Settings()
---
+++ api/migrations/versions/8aceb829f61b_models_definition.py
@@ -0,0 +1,76 @@
+"""Models definition
+
+Revision ID: 8aceb829f61b
+Revises: 
+Create Date: 2024-03-25 21:46:42.214491
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import text
+
+# revision identifiers, used by Alembic.
+revision: str = "8aceb829f61b"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    connection = op.get_bind()
+
+    connection.execute(
+        text(
+            """
+    CREATE TABLE IF NOT EXISTS
+    repositories(
+    id bigserial primary key,
+    repo varchar(512) unique not null,
+    owner varchar(256) not null,
+    position_cur integer not null,
+    position_prev integer default null,
+    stars integer not null,
+    watchers integer not null,
+    forks integer not null,
+    open_issues integer not null,
+    language varchar(128)
+    );
+    """
+        )
+    )
+    connection.execute(
+        text(
+            """
+    CREATE TABLE IF NOT EXISTS
+    repo_analytics(
+    id bigserial primary key,
+    repo_id integer,
+    date date not null,
+    commits int not null,
+    authors varchar(256)[],
+    foreign key(repo_id) references repositories(id) on delete cascade
+    );
+    """
+        )
+    )
+
+
+def downgrade() -> None:
+    connection = op.get_bind()
+
+    connection.execute(
+        text(
+            """
+    DROP TABLE IF EXISTS repositories CASCADE;
+    """
+        )
+    )
+    connection.execute(
+        text(
+            """
+    DROP TABLE IF EXISTS repo_analytics CASCADE;
+    """
+        )
+    )
