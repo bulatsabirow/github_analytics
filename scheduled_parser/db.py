3595e9bed522461c0d461e2414498e8defcf378d
bulatsabirow <bulatsabirow@gmail.com>
2024-04-11 12:38:59 +0300

    ! fix database connection error in cloud function

--- scheduled_parser/db.py
+++ scheduled_parser/db.py
@@ -2,6 +2,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
 def get_async_session_maker(database_url: str):
-    engine = create_async_engine(url=database_url, echo=True)
-    async_session_maker = async_sessionmaker(engine, autoflush=False)
+    engine = create_async_engine(url=database_url, echo=True, connect_args={"statement_cache_size": 0})
+    async_session_maker = async_sessionmaker(engine, autoflush=False, expire_on_commit=False, autocommit=False)
     return async_session_maker
